31a1534e2dac056c9490d7e03d456c4c7c2d93f7
hrvieira <luizhrvieira@outlook.com>
2024-02-17 21:44:57 -0300

    Melhorando a responsividade do botão de menu

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu() {
+function toggleMenu(event) {
+  if(event.type === 'touchstart') event.preventDefaul();
   const nav = document.getElementById('nav');
   // classList = listar todas as classes
   // toggle = adicione caso não tenha / remova caso tenha
@@ -8,3 +9,6 @@ function toggleMenu() {
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+/* Quando o evento de click é usado em um site mobile tem um atraso de 300ms, trasendo um pouco de atraso.
+O menu vai ser aberto, mas não é tão responsivo */
+btnMobile.addEventListener('touchstart', toggleMenu);
